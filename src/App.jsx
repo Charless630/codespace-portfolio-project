5f89c04af0e730454375ccfb400684ec7d83860f
kaszapm <131197895+Charless630@users.noreply.github.com>
2023-12-04 15:28:25 +0000

    set homepage for github repo

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mario Kaszap",
+  title: "Beginner Full Stack Developer",
+  email: "kaszapmario7@gmail.com",
+  gitHub: "https://github.com/Charless630",
+  instagram: "",
+  linkedIn: "www.linkedin.com/in/mario-kaszap-207178235",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
